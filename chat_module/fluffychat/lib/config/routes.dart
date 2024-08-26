1e86530eff9da14de0fc2087131d23fda9206777
Ruslan Nikolayev <morfed.quarezz@gmail.com>
2024-08-26 17:47:10 +0300

    Implemented Contacts page, added fetching of contacts and avatars

--- chat_module/fluffychat/lib/config/routes.dart
+++ chat_module/fluffychat/lib/config/routes.dart
@@ -102,7 +102,7 @@ abstract class AppRoutes {
           pageBuilder: (context, state) => defaultPageBuilder(
               context,
               state,
-              const ContactList(contacts: []),
+              ContactList(),
           ),
         ),
         GoRoute(
--- chat_module/fluffychat/lib/pages/home/contacts.dart
+++ chat_module/fluffychat/lib/pages/home/contacts.dart
@@ -1,35 +1,67 @@
+import 'package:fluffychat/pages/home/contacts_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:fluffychat/widgets/avatar.dart';
+import 'package:matrix/matrix_api_lite/generated/model.dart';
 
-class Contact {
-  final String name;
-  final Uri? avatarUrl;
-  final String userId;
-
-  Contact(this.avatarUrl, this.userId, {required this.name});
-}
+import '../../widgets/matrix.dart';
+import '../../widgets/mxc_image.dart';
 
 class ContactList extends StatelessWidget {
-  final List<Contact> contacts;
+  final ContactsController controller = ContactsController();
 
-  const ContactList({super.key, required this.contacts});
+  ContactList({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: contacts.length,
-      itemBuilder: (context, i) {
-        final contact = contacts[i];
-        return ListTile(
-          leading: Avatar(
-            name: contact.name,
-            mxContent: contact.avatarUrl,
-            presenceUserId: contact.userId,
-          ),
-          title: Text(contact.name),
-          subtitle: Text(contact.userId),
-        );
-      },
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Контакти'),
+      ),
+      body: FutureBuilder<List<Contact>>(
+        /// Fetch contacts
+        future: controller.fetchContacts(context),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return Center(child: Text('No contacts found.'));
+          }
+          /// Sort fetched contacts
+          final contacts = snapshot.data!;
+          /// Present contacts on the UI
+          return ListView.builder(
+            itemCount: contacts.length,
+            itemBuilder: (context, index) {
+              final contact = contacts[index];
+              return ListTile(
+                leading: ClipOval(
+                  child: SizedBox(
+                    width: 64.0, // Specify the width
+                    height: 64.0, // Specify the height
+                    child: Center(child: MxcImage(
+                        key: Key(contact.userId),
+                        uri: contact.avatarUrl,
+                        fit: BoxFit.fill,
+                        width: 64,
+                        height: 64,
+                        placeholder: (_) => Container(color: Colors.grey),
+                        cacheKey: contact.userId.toString(),
+                      )
+                    ),
+                  ),
+                ),
+                title: Text(contact.name),
+                subtitle: Text(contact.userId),
+                onTap: () {
+                  // Handle contact tap, e.g., navigate to chat
+                },
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
